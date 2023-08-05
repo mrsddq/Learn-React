c58125795d1282cbe2a138cfa16575adfad59e9f
mrsddq <laraibks@gmail.com>
2023-08-05 15:27:57 +0530

    Creating an Element in Javascript

---
+++ Introduction to React/index.js
@@ -0,0 +1,8 @@
+//Creating an Element in Javascript
+
+const heading = document.createElement("h2");
+heading.textContent="Hello World";
+heading.className = "Header";
+document.getElementById("root").append(heading);
+
+//Creating an Element in Javascript
